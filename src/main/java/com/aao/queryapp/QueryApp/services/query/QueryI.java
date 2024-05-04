d80602440400ed98284d9c52442bf7834f2f9785
adriel arocha <adrielarochaoronoz@gmail.com>
2024-05-04 22:26:30 +0000

    :recycle: new query implementation

--- src/main/java/com/aao/queryapp/QueryApp/services/query/QueryI.java
+++ src/main/java/com/aao/queryapp/QueryApp/services/query/QueryI.java
@@ -4,9 +4,6 @@ import com.aao.queryapp.QueryApp.Entities.Models;
 import java.util.*;
 
 public interface QueryI {
-    String createQuery(Models parentModel);
     String createQuery();
-    String createQuery(Models parentModel, Collection<Models> childrenModels);
-    String selectPhase(Models parentModel);
-    String joinPhase(Models parentModel, Models childModel);
 }
+
